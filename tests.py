edf20afa50aff4219ddc4c270e07db2ca629b8c7
Motoki Naruse <motoki@naru.se>
2018-11-03 14:51:10 +0900

    ファクトリ関数経由でSemverを作るようにした

--- tests.py
+++ tests.py
@@ -8,7 +8,7 @@ import pytest
     (("2", "1", "1"), "2.1.1"),
 ])
 def test_文字列表現(source, expect):
-    assert str(semver.Semver(*source)) == expect
+    assert str(semver.Semver.create(*source)) == expect
 
 
 @pytest.mark.parametrize("source,other,expect", [
@@ -16,6 +16,4 @@ def test_文字列表現(source, expect):
     (("2", "1", "1"),("3", "5", "4"), False),
 ])
 def test_バージョン比較(source, other, expect):
-    assert (semver.Semver(*source) == semver.Semver(*other)) == expect
-    
-
+    assert (semver.Semver.create(*source) == semver.Semver.create(*other)) == expect
