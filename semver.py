b3eea8fcaef7cb117fbab118715dec3a1878f271
Jun Nakajima <ikuta1919@gmail.com>
2018-11-03 15:07:48 +0900

    引数をint型に変更

--- semver.py
+++ semver.py
@@ -2,9 +2,22 @@ from typing import NamedTuple
 
 
 class Semver(NamedTuple):
-    major: str
-    minor: str
-    patch: str
+    major: int
+    minor: int
+    patch: int
 
     def __str__(self) -> str:
-        return ".".join(self)
+        return f"{self.major}.{self.minor}.{self.patch}"
+
+    @classmethod
+    def create(cls, major, minor, patch):
+        if not isinstance(major, int):
+            raise TypeError()
+
+        if not isinstance(minor, int):
+            raise TypeError()
+
+        if not isinstance(patch, int):
+            raise TypeError()
+
+        return cls(major, minor, patch)
--- tests.py
+++ tests.py
@@ -4,16 +4,16 @@ import pytest
 
 
 @pytest.mark.parametrize("source,expect", [
-    (("1", "4", "2"), "1.4.2"),
-    (("2", "1", "1"), "2.1.1"),
+    ((1, 4, 2), "1.4.2"),
+    ((2, 1, 1), "2.1.1"),
 ])
 def test_文字列表現(source, expect):
     assert str(semver.Semver.create(*source)) == expect
 
 
 @pytest.mark.parametrize("source,other,expect", [
-    (("1", "4", "2"),("1", "4", "2"), True),
-    (("2", "1", "1"),("3", "5", "4"), False),
+    ((1, 4, 2),(1, 4, 2), True),
+    ((2, 1, 1),(3, 5, 4), False),
 ])
 def test_バージョン比較(source, other, expect):
     assert (semver.Semver.create(*source) == semver.Semver.create(*other)) == expect
